b79d445f788f83e3b4657e4abe05c1e00f3e45e0
Su14iman <semoo@dr.com>
2021-01-23 14:12:01 +0100

    fix 2 agin

--- assets/layouts/navbarSales.php
+++ assets/layouts/navbarSales.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-dark blue">
-  <a class="navbar-brand" id="nav-header" href="main.php">Sinema_vtys</a>
+  <a class="navbar-brand" id="nav-header" href="tickets.php">Sinema_vtys</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
