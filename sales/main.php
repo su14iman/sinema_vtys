ff0df5514828a8898fc5c151356d301fd708a5bb
Su14iman <semoo@dr.com>
2021-01-23 13:35:58 +0100

    fix main page in sales section

--- sales/main.php
+++ sales/main.php
@@ -5,7 +5,8 @@ include("./session.php");
     include('../assets/layouts/hedaerSales.php'); // head require
     include('../assets/layouts/navbarSales.php'); // nav require
 
-        include("../assets/pages/salse/tickets.php"); // content require
+        // include("../assets/pages/salse/tickets.php"); // content require
+        header('Location: tickets.php');
 
     include('../assets/layouts/footerSales.php'); // footer require
 
